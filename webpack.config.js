d1e144f9cc17f678cac618319f571df3fd70398f
hyy <hyyfrank@gmail.com>
2019-01-31 01:23:57 +0800

    update and test some option in html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,25 @@ module.exports = {
       filename:'index.html',
       hash:true,//防止缓存
       minify:{
-          removeAttributeQuotes:true//压缩 去掉引号
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+        removeAttributeQuotes:true//压缩 去掉引号
+      },
+      meta: {
+        'viewport': 'width=device-width, initial-scale=1, shrink-to-fit=no',
+        // Will generate: <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        'theme-color': '#4285f4',
+        // Will generate: <meta name="theme-color" content="#4285f4">
+        // 'Content-Security-Policy': { 'http-equiv': 'Content-Security-Policy', 'content': 'default-src https:' },
+      // Will generate: <meta http-equiv="Content-Security-Policy" content="default-src https:">
+      // Which equals to the following http header: `Content-Security-Policy: default-src https:`
+      // 'set-cookie': { 'http-equiv': 'set-cookie', content: 'name=value; expires=date; path=url' },
+      // Will generate: <meta http-equiv="set-cookie" content="value; expires=date; path=url">
+      // Which equals to the following http header: `set-cookie: value; expires=date; path=url`
       }
     })
   ]

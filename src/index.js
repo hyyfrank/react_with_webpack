95877deae390e7f4e5bd2f2484a03a4d97e3b924
hyy <hyyfrank@gmail.com>
2019-01-30 22:54:48 +0800

    add html-webpack-plugin

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
 import addtwo from "./add"
 console.log("javascript say hello2.")
 addtwo(1,2)
-
-if(module.hot){
-    module.hot.accept();
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,24 @@
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: [
-    "./src/main.js",
-    "webpack/hot/dev-server",
-    "webpack-dev-server/client?http://localhost:8080"
+    "./src/index.js"
   ],
   output: {
     publicPath: "",
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
   },
-  mode: "development", 
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
+  mode: "development",
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname,'src','index.html'),//模板
+      filename:'index.html',
+      hash:true,//防止缓存
+      minify:{
+          removeAttributeQuotes:true//压缩 去掉引号
+      }
+    })
   ]
 };

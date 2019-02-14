b0dcb902e9beb4cf50853c4c0737d174275b7ce1
Huang Yanyi <hyy@MACPC0GU18Q.ads.autodesk.com>
2019-02-15 02:13:18 +0800

    ⚡ add css support,and scss support,need fix postcss

--- webpack.config.js
+++ webpack.config.js
@@ -14,12 +14,6 @@ const cssDevRules=[
   },
   {
     loader: 'postcss-loader',
-    options: {
-      plugins: () => [
-        require('autoprefixer')
-      ],
-      sourceMap: true
-    }
   },
   {
     loader:'sass-loader',

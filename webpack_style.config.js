171c225ec2fe4d7cdc2717868afc807edf70f4b0
Frank-Yanyi Huang <frank-yanyi.huang@autodesk.com>
2019-02-18 16:56:30 +0800

    make production work

--- webpack_style.config.js
+++ webpack_style.config.js
@@ -1,4 +1,3 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isProd = process.env.NODE_ENV === 'production';
 console.log("process node env is:"+process.env.NODE_ENV)
@@ -18,12 +17,13 @@ const cssProdRules=[
         loader:'style-loader'
     },
     {
-        loader:'css-loader',
+        loader:'css-loader?modules&localIdentName=[name]_[local]_[hash:base64:5]',
     },
     {
         loader:'sass-loader',
     }
 ];
+
 console.log("is prod:"+isProd);
 const baseConfig = {
     module: {
@@ -36,10 +36,7 @@ const baseConfig = {
         ]
     },
     plugins: [
-        new MiniCssExtractPlugin({
-            filename: isProd ?  '[name].[hash].css':'[name].css',
-            chunkFilename: isProd ? '[id].[hash].css':'[id].css',
-        }),
+
     ],
 };
 module.exports = baseConfig;

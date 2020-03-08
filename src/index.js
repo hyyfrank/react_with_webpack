e680cb41f7bf30deed3ab2e599d70872ecc5bda3
frank <hyyfrank@gmail.com>
2020-03-08 19:46:28 +0800

    add redux customize middleware

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import { Provider, connect } from "react-redux";
 import logger from "redux-logger"
 
 import HomeComponent from "./components/home";
+import CustomerMiddle from "./components/middlewares/customer"
 import myreducer from "./components/reducer/myreducer"
 import increaseAction from "./components/actions/increaseAction"
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const middleware = [logger]
+const middleware = [CustomerMiddle,logger]
 const composeEnhancers = composeWithDevTools(
     applyMiddleware(...middleware)
 );

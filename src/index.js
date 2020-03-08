5cf0711795a4f675f6586b42c4416203aea7c244
frank <hyyfrank@gmail.com>
2020-03-08 19:40:27 +0800

    add redux dev tool and compose with middleware

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { createStore } from "redux";
+import { createStore,applyMiddleware } from "redux";
 import { Provider, connect } from "react-redux";
+import logger from "redux-logger"
 
 import HomeComponent from "./components/home";
 import myreducer from "./components/reducer/myreducer"
 import increaseAction from "./components/actions/increaseAction"
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(myreducer);
+const middleware = [logger]
+const composeEnhancers = composeWithDevTools(
+    applyMiddleware(...middleware)
+);
+const store = createStore(myreducer,composeEnhancers);
 // Map Redux state to component props
 function mapStateToProps(state) {
   return {

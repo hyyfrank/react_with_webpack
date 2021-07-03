7ea316d347da91506a6ba7e4b7dc0e0cb8c6d2c9
hyy <hyy@hyys-MacBook-Pro.local>
2021-07-03 21:38:19 +0800

    add the layout page

--- src/common/Layout.jsx
+++ src/common/Layout.jsx
@@ -1,33 +1,64 @@
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Dropdown } from 'antd';
 import React, { Component } from "react";
 import * as style from '../css/layout.less';
 const { Header, Content, Sider, Footer} = Layout;
-
 import {
+  BgColorsOutlined,
   MenuUnfoldOutlined,
   MenuFoldOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
+  PieChartOutlined,
+  DesktopOutlined,
+  ContainerOutlined,
+  MailOutlined,
+  AppstoreOutlined,
+  DownOutlined
 } from '@ant-design/icons';
+const { SubMenu } = Menu;
+
 
 export default class LayoutComponent extends Component {
     constructor(){
         super();
         this.state = {
             collapsed: false,
+            current: 'mail',
         };
         this.toggle = this.toggle.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
     toggle(){
         this.setState({
             collapsed: !this.state.collapsed,
         });
     }
+    handleClick(){
+      console.log('click ', e);
+      this.setState({ current: e.key });
+    }
     shouldComponentUpdate(){
       return true;
     }
 	render() {
+    const menu = (
+      <Menu
+        defaultSelectedKeys={['1']}
+        defaultOpenKeys={['1']}
+        mode="inline"
+        theme="light"
+        inlineCollapsed={this.state.collapsed}
+      >
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+            <a href="#">个人中心</a>
+          </Menu.Item>
+          <Menu.Item key="2" icon={<DesktopOutlined />}>
+          <a href="#">个人设置</a>
+          </Menu.Item>
+          <Menu.Item key="3" icon={<DesktopOutlined />}>
+          <a href="#">退出登录</a>
+          </Menu.Item>
+      </Menu>
+    );
+    
 		return <Layout className={style.layoutContainer}>
               <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
                 <div className={style.topLogo}>
@@ -37,16 +68,43 @@ export default class LayoutComponent extends Component {
                       }  
                     </div>
                 </div>
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                  <Menu.Item key="1" icon={<UserOutlined />}>
-                    nav 1
+                <Menu
+                  defaultSelectedKeys={['1']}
+                  defaultOpenKeys={['sub1']}
+                  mode="inline"
+                  theme="dark"
+                  inlineCollapsed={this.state.collapsed}
+                >
+                  <Menu.Item key="1" icon={<PieChartOutlined />}>
+                    Dashboard
                   </Menu.Item>
-                  <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-                    nav 2
+                  <Menu.Item key="2" icon={<DesktopOutlined />}>
+                    Algorithms
                   </Menu.Item>
-                  <Menu.Item key="3" icon={<UploadOutlined />}>
-                    nav 3
+                  <Menu.Item key="3" icon={<ContainerOutlined />}>
+                    Videos
                   </Menu.Item>
+                  <Menu.Item key="4" icon={<BgColorsOutlined />}>
+                    Configs
+                  </Menu.Item>
+                  <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
+                    <Menu.Item key="5">Option 5</Menu.Item>
+                    <Menu.Item key="6">Option 6</Menu.Item>
+                    <Menu.Item key="7">Option 7</Menu.Item>
+                    <Menu.Item key="8">Option 8</Menu.Item>
+                    <Menu.Item key="55">Option 5</Menu.Item>
+                    <Menu.Item key="66">Option 6</Menu.Item>
+                    <Menu.Item key="77">Option 7</Menu.Item>
+                    <Menu.Item key="88">Option 8</Menu.Item>
+                  </SubMenu>
+                  <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">
+                    <Menu.Item key="9">Option 9</Menu.Item>
+                    <Menu.Item key="10">Option 10</Menu.Item>
+                    <SubMenu key="sub3" title="Submenu">
+                      <Menu.Item key="11">Option 11</Menu.Item>
+                      <Menu.Item key="12">Option 12</Menu.Item>
+                    </SubMenu>
+                  </SubMenu>
                 </Menu>
                 <div className={style.bottomLink}>
                   {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
@@ -56,8 +114,13 @@ export default class LayoutComponent extends Component {
                 </div>
               </Sider>
               <Layout className="site-layout">
-                <Header className="site-layout-background" style={{ padding: 0 }}>
-                  
+                <Header className="site-layout-background topdropdown" style={{ padding: 0 }}>
+                  <Dropdown overlay={menu} arrow={false}>
+                    <a className="ant-dropdown-link headermenuright" onClick={e => e.preventDefault()}>
+                      <div className={style.userLogo}></div>
+                      <div>Serati Ma</div>
+                    </a>
+                  </Dropdown>
                 </Header>
                 <Content
                   className="site-layout-background"
@@ -67,9 +130,8 @@ export default class LayoutComponent extends Component {
                     minHeight: 280,
                   }}
                 >
-                  Content
-                </Content>
-                
+                  {this.props.children}
+                </Content>     
               </Layout>
             </Layout>
 			
